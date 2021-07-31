f364c78a2bf8864370d3b1b93f97b78d861197e6
arily <arily@arily.moe>
2021-07-31 19:02:35 +0900

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: 'api',
+      name: 'osu-collection-share/api',
       script: 'npm',
       args: 'run express-api'
     },
     {
-      name: 'frontend',
+      name: 'osu-collection-share/frontend',
       script: 'npm',
       exec_mode: 'cluster',
       args: 'start'
