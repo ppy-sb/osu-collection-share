503457a23ea899ec2b70783b18786adcbf41636d
arily <arily@arily.moe>
2021-08-03 15:32:46 +0900

    prod

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,15 @@ module.exports = {
     {
       name: 'osu-collection-share/api',
       script: 'npm',
-      args: 'run express-api'
+      args: 'run express-api',
+      exec_mode: 'cluster',
+      instances: 4
     },
     {
       name: 'osu-collection-share/frontend',
       script: 'npm',
       exec_mode: 'cluster',
+      instances: 4,
       args: 'start'
     }
   ]
