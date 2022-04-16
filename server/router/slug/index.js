c9a6b61f5904f2d6697a03872445ab99fb6bc004
arily <arily@arily.moe>
2022-04-17 01:53:25 +0900

    remove unused deps & fix slug calc

--- server/router/slug/index.js
+++ server/router/slug/index.js
@@ -1,9 +1,14 @@
+const express = require('express')
 const models = require('~models')
 const getSlug = require('~controllers/slug')
-module.exports = async (req, res, next) => {
-  const target = req.params[0] || undefined
+const router = express.Router()
+
+router.get('/:slug', async (req, res, next) => {
+  const target = req.params.slug || undefined
   // if (target === undefined) { next(Error('slug required')) }
   const model = models.CollectionDB
   const result = await getSlug(target, model)
   res.json(result)
-}
+})
+
+module.exports = router
